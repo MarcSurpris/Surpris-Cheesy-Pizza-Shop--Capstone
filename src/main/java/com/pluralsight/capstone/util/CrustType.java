2e5bdd8fcfdb07560c6b829c98ed1e30b8269e79
MarcSurpris <MarcSurpris7@gmail.com>
2025-11-10 11:38:14 -0500

    This java class CrustType defines public static final String constants representing different piza crust options.

--- src/main/java/com/pluralsight/capstone/util/CrustType.java
+++ src/main/java/com/pluralsight/capstone/util/CrustType.java
@@ -2,4 +2,9 @@ package com.pluralsight.capstone.util;
 
 // CrustType class with constants
 public class CrustType {
+    public static final String THIN = "THIN";
+    public static final String REGULAR = "REGULAR";
+    public static final String THICK = "THICK";
+    public static final String CAULIFLOWER = "CAULIFLOWER";
+    // All same price per size, no diff.
 }
