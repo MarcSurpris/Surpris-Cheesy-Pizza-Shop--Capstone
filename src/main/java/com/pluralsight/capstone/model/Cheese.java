7f04f748b55a6e1bdb8a2c6cb9ea32fe42af4cc8
MarcSurpris <MarcSurpris7@gmail.com>
2025-11-12 10:03:53 -0500

    This method overrides the abstract getBasePrice(String size) from the Topping class in the Cheese subclass. This returns the standard price for one portion of any cheese topping based on the pizza size.

--- src/main/java/com/pluralsight/capstone/model/Cheese.java
+++ src/main/java/com/pluralsight/capstone/model/Cheese.java
@@ -1,10 +1,16 @@
 package com.pluralsight.capstone.model;
 
+import com.pluralsight.capstone.util.PizzaSize;
 import com.pluralsight.capstone.util.ToppingCategory;
 
 // Cheese Topping
 public class Cheese extends Topping {
     public Cheese(String name) {
         super(name, ToppingCategory.CHEESE);
+    }
+
+    @Override
+    public double getBasePrice(String size) {
+        return PizzaSize.getMeatPrice(size);
     }
 }
