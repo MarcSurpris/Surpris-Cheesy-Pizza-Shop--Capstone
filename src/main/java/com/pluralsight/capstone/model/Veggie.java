72d1e77fd0a0396027149def8affa39189e036fd
MarcSurpris <MarcSurpris7@gmail.com>
2025-11-13 11:26:42 -0500

    This is the default constructor for the Veggie class (a subclass of Pizza). Creates a pre-configured vegetarian pizza with standard defaults.

--- src/main/java/com/pluralsight/capstone/model/Veggie.java
+++ src/main/java/com/pluralsight/capstone/model/Veggie.java
@@ -2,5 +2,17 @@ package com.pluralsight.capstone.model;
 
 // Veggie
 
+import com.pluralsight.capstone.util.CrustType;
+import com.pluralsight.capstone.util.PizzaSize;
+
 public class Veggie extends Pizza {
+    public Veggie() {
+        super(PizzaSize.PERSONAL_8_INCH, CrustType.REGULAR);
+        addTopping(new RegularTopping("Bell Peppers"));
+        addTopping(new RegularTopping("Spinach"));
+        addTopping(new RegularTopping("Olives"));
+        addTopping(new RegularTopping("Onions"));
+        addTopping(new Sauce("Marinara"));
+        addTopping(new Cheese("Mozzarella"));
+    }
 }
