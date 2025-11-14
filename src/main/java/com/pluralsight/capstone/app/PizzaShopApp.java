f8a47544322ea933230c0bfce3f0bcda99a45212
MarcSurpris <MarcSurpris7@gmail.com>
2025-11-13 19:07:08 -0500

    Main entry point, fields are static Scanner scanner, for reading user input from the console and static Order currentOrder, holds the active order during a session. It's running a while (running) loop. Also using a switch statement.

--- src/main/java/com/pluralsight/capstone/app/PizzaShopApp.java
+++ src/main/java/com/pluralsight/capstone/app/PizzaShopApp.java
@@ -1,7 +1,33 @@
 package com.pluralsight.capstone.app;
 
+import com.pluralsight.capstone.model.Order;
+
 import java.util.Scanner;
 
 // Main Application
 public class PizzaShopApp {
+    private static Scanner = new Scanner(System.in);
+    private static Order currentOrder;
+
+    public static void main(String[] args) {
+        // Ensure receipts folder exists, assume it does or create.
+
+        boolean running = true;
+        while (running) {
+            displayHomeScreen();
+            int choice = getIntInput();
+            switch (choice) {
+                case 1:
+                    currentOrder  = new Order();
+                    handleOrderScreen();
+                    break;
+                case 0:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        }
+        System.out.println("Exiting application");
+    }
 }
